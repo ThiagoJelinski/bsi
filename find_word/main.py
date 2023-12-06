b99d9cdfc60475125ea70a26547d9b0e39824459
ThiagoJelinski <thi.jel@outlook.com>
2023-12-06 10:22:05 -0300

    Exercício de revisão.

---
+++ find_word/main.py
@@ -0,0 +1,12 @@
+from game_find_word import FindTheWord
+
+
+def main():
+    find_name = FindTheWord()
+    find_name.infos()
+    words = find_name.choice_level()
+    find_name.shuffle_words(words)
+
+
+if __name__ == "__main__":
+    main()
