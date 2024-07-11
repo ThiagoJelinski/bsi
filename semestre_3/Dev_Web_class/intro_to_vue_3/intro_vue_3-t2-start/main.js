a3e896ad168f546d9c1856e1a912a450203074d9
Thiago Jelinski <thi.jel@outlook.com>
2024-07-11 10:42:02 -0300

    Redenrização de Lista

--- semestre_3/Dev_Web_class/intro_to_vue_3/intro_vue_3-t2-start/main.js
+++ semestre_3/Dev_Web_class/intro_to_vue_3/intro_vue_3-t2-start/main.js
@@ -3,8 +3,14 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
-            inventory: 0,
+            inventory: 5,
             onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' },
+            ],
+            sizes: ['S','M','L','XL'],
             url: 'https://github.com/csp1po/intro_vue_3/tree/t3-start'
         }
     }
