747792f4a34dcb2bd576edb173d6396b80e49304
Thiago Jelinski <thi.jel@outlook.com>
2024-07-11 09:50:56 -0300

    Renderização Condicional

--- semestre_3/Dev_Web_class/intro_to_vue_3/intro_vue_3-t2-start/main.js
+++ semestre_3/Dev_Web_class/intro_to_vue_3/intro_vue_3-t2-start/main.js
@@ -3,6 +3,8 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
+            inventory: 0,
+            onSale: true,
             url: 'https://github.com/csp1po/intro_vue_3/tree/t3-start'
         }
     }
